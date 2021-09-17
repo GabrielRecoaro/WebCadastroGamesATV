0e9ab207251326802840a028a3533979b25ade74
gabrielrecoaro <gabriel.recoaro63@gmail.com>
2021-09-17 10:10:45 -0300

    Criação da Acoes

---
+++ WebCadastroGames/Repositorio/Acoes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WebCadastroGames.Repositorio;
+using MySql.Data.MySqlClient;
+using WebCadastroGames.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebCadastroGames.Repositorio
+{
+    public class Acoes
+    {
+        Conexao con = new Conexao();
+        MySqlCommand cmd = new MySqlCommand();
+
+        public void CadastrarFuncionario(Funcionario func)
+        {
+            string data_sistema = Convert.ToDateTime(func.FuncDtNasc).ToString("yyyy-MM-dd");
+            cmd.Parameters.Add("@funcCod", MySqlDbType.VarChar).Value = FuncCod;
+        }
+    }
+}
